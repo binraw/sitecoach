55214385709cdc805d62ced7df4fdf071bc9128b
binraw <truvelotrobin@yahoo.fr>
2023-04-20 16:31:22 +0200

    ajout effet

--- dossier.js/mainmaj.js
+++ dossier.js/mainmaj.js
@@ -37,6 +37,7 @@ anime({
 	duration: 1200,
 	opacity: [0, 1],
 	delay: 1000,
+	easing: "easeOutQuint",
 });
 anime({
 	targets: ".first-title",
@@ -45,17 +46,39 @@ anime({
 	opacity: [0, 1],
 	delay: 300,
 });
-anime({
-	targets: ".introduction",
-	translateX: [100, 0],
-	duration: 1200,
-	opacity: [0, 1],
-	delay: 1600,
-});
+
 anime({
 	targets: ".bar-nav",
 	opacity: [0, 1],
 	duration: 1200,
 
 	delay: 900,
+});
+
+// cette const permet de definir 10%
+const ratio = 0.1;
+const options = {
+	root: null,
+	rootMargin: "0px",
+	threshold: ratio,
+};
+const handleIntersect = function (entries, observer) {
+	entries.forEach(function (entry) {
+		if (entry.intersectionRatio > ratio) {
+			entry.target.classList.add("visible");
+			observer.unobserve(entry.target);
+		}
+	});
+};
+const observer = new IntersectionObserver(handleIntersect, options);
+observer.observe(document.querySelector(".title-pull"));
+
+// COnstruction function pour faire l'effet curseur
+
+document.addEventListener("DOMContentLoaded", function () {
+	const curseur = document.querySelector(".cursor");
+	document.addEventListener("mousemove", function (e) {
+		curseur.style.cssText =
+			"left: " + e.clientX + "px; top: " + e.clientY + "px;";
+	});
 });
